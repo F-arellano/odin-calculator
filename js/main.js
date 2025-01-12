257c83c2cafb64cb83bb019c116cd6ded53e55cd
F-arellano <pipe.arellano98@gmail.com>
2025-01-12 09:55:18 -0300

    ADD: implementation of multiplication and division
    + the logic is encapsulated in handleEqual
    - TODO:
        implementation of adition/substraction
        parentheses
        dot

--- js/main.js
+++ js/main.js
@@ -1,40 +1,116 @@
+idValues = {
+    "b0": "0",
+    "b1": "1",
+    "b2": "2",
+    "b3": "3",
+    "b4": "4",
+    "b5": "5",
+    "b6": "6",
+    "b7": "7",
+    "b8": "8",
+    "b9": "9",
+    "times": "*",
+    "division": "/",
+    "plus": "+",
+    "minus": "-",
+    "undo": "C",
+    "reset": "R",
+    "left-parenthesis": ")",
+    "right-parenthesis": "(",
+    "equal": "=",
+    "dot": "."
+}
+
 
 function updateInput(e) {
     const input = document.querySelector('#input');
     const output = document.querySelector("#output")
 
-    let text = ""
-    switch (e.target.id) {
-        case "b0": text = "0"; break
-        case "b1": text = "1"; break
-        case "b2": text = "2"; break
-        case "b3": text = "3"; break
-        case "b4": text = "4"; break
-        case "b5": text = "5"; break
-        case "b6": text = "6"; break
-        case "b7": text = "7"; break
-        case "b8": text = "8"; break
-        case "b9": text = "9"; break
-        case "left-parenthesis": text = "("; break
-        case "right-parenthesis": text = ")"; break
-        case "percentage": text = "%"; break
-        case "division": text = "/"; break
-        case "times": text = "*"; break
-        case "plus": text = "+"; break
-        case "minus": text = "-"; break
-        case "dot": text = "."; break
-        case "equal": text = "="; break
-        case "reset": clearInput(); break
-    }
-    input.textContent += text
-    output.textContent += text
+    const char = idValues[e.target.id]
+
+    handleDigits(char) 
+    handleOperators(char)
+    handleReset(char)
+    handleEqual(char)
+    console.log("update!")
 }
 
-function clearInput() {
+function handleDigits(char) {
+    // check if is digit. automatically try to convert char to number
+    if (!(char > -1)) return 
+
+    const input = document.querySelector("#input")
+    input.textContent += char
+}
+
+function handleOperators(char) {
+    if (!isOperator(char)) return 
+
+    const input = document.querySelector("#input")
+    const lastChar = input.textContent.slice(-1)
+
+    if (isOperator(lastChar)) {
+        input.textContent = input.textContent.slice(0, -1)
+    }
+
+    input.textContent += char
+}
+
+function handleReset(char) {
+    if (char !== "R") return
+
     const input = document.querySelector("#input")
     const output = document.querySelector("#output")
     input.textContent = ""
     output.textContent = ""
+}
+
+function handleEqual(char) {
+    if (char !== "=") return 
+
+    const output = document.querySelector("#output")
+    const input = document.querySelector("#input")
+    const formula = input.textContent
+
+    // get an array with all numbers and another with all operations
+    let numbers = formula.split( /[*\/+-]/ )
+    numbers = numbers.map(item => Number(item))
+
+    let operators = formula.split( /[0-9]/ )
+    operators = operators.filter(item => item !== "")
+
+    // solving using PAPOMUDAS
+    while (operators.includes("*") || operators.includes("/")) {
+        // get operator index
+        const i = operators.findIndex(item => item == "*" || item == "/")
+        let result
+
+        if (operators[i] == "*") {
+            result = numbers[i] * numbers[i+1]
+        } else if (operators[i] == "/") {
+            result = numbers[i] / numbers[i+1]
+        }
+
+        // update arrays
+        numbers.splice(i, 1)
+        numbers[i] = result 
+        operators.splice(i, 1)
+    }
+
+    output.textContent = numbers[0]
+}
+
+function isOperator(char) {
+    if (
+        char === "+" ||
+        char === "-" ||
+        char === "/" ||
+        char === "*" ||
+        char === "%"
+    ) {
+        return true
+    }
+    return false
 }
 
 const container = document.querySelector(".container")
